75c9b76f9c354737b28f2f4df83b27a0db7110de
Jose Garcia <josegarcia19809@gmail.com>
2026-05-16 18:01:10 -0600

    Agregando señales

--- bases/src/app/pages/counter/counter-page.ts
+++ bases/src/app/pages/counter/counter-page.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter-page',
-  template: `
-    <h1>Hola mundo {{ 1 + 2 }}</h1>
-    <p>Counter: {{ counter }}</p>
-    <button (click)="increaseBy(1)" )>+1</button>
-  `,
+  templateUrl: './counter-page.html',
+  styleUrls: ['./counter-page.css'],
 })
 export class CounterPageComponent {
   counter = 10;
+  counterSignal = signal(20)
 
   increaseBy(value: number) {
     this.counter += value;
+    this.counterSignal.update((currentValue) => currentValue + value);
+  }
+
+  decreaseBy(value: number) {
+    this.counter -= value;
+    this.counterSignal.update((currentValue) => currentValue - value);
+  }
+
+  reset(){
+    this.counter = 0;
+    this.counterSignal.set(0);
   }
 }
