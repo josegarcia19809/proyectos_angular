448fbd2e22b0d0f342d88a0e3f1de544d053ab1c
Jose Garcia <josegarcia19809@gmail.com>
2026-05-21 20:22:56 -0600

    Angular Total - Proyecto inicial:  Mostrar Contenido Dinámico con Interpolación de Cadenas

--- proyecto-inicial-angular-total/src/app/app.ts
+++ proyecto-inicial-angular-total/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Encabezado } from './components/encabezado/encabezado';
+import { Usuario } from './components/usuario/usuario';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Encabezado],
+  imports: [RouterOutlet, Encabezado, Usuario],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('proyecto-inicial-angular-total');
---
+++ proyecto-inicial-angular-total/src/app/components/usuario/usuario.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { USUARIOS_FALSOS } from '../../usuarios-falsos';
+
+const indiceAleatorio = Math.floor(Math.random() * USUARIOS_FALSOS.length);
+@Component({
+  selector: 'app-usuario',
+  imports: [],
+  templateUrl: './usuario.html',
+  styleUrl: './usuario.css',
+})
+export class Usuario {
+  usuarioSeleccionado = USUARIOS_FALSOS[indiceAleatorio];
+}
