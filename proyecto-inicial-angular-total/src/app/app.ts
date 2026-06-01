6fdf257405043d9ec2ae6ccf413cfb22b30f9efc
Jose Garcia <josegarcia19809@gmail.com>
2026-06-01 16:08:00 -0600

    Angular Total - Proyecto inicial: Ejercicio: Crea un Componente Configurable en Angular

--- proyecto-inicial-angular-total/src/app/app.ts
+++ proyecto-inicial-angular-total/src/app/app.ts
@@ -3,18 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import { Encabezado } from './components/encabezado/encabezado';
 import { Usuario } from './components/usuario/usuario';
 import {USUARIOS_FALSOS} from './usuarios-falsos';
+import { Tareas } from './components/tareas/tareas';
 
 @Component({
   selector: 'app-root',
-  imports: [Encabezado, Usuario],
+  imports: [Encabezado, Usuario, Tareas],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('proyecto-inicial-angular-total');
-  usuarios= USUARIOS_FALSOS;
+  usuarios = USUARIOS_FALSOS;
+  idUsuarioSeleccionado ="u1"
 
-  alSeleccionarUsuario(id: string){
-    console.log(id);
+  get usuarioSeleccionado(){
+    return this.usuarios.find((usuario) => usuario.id === this.idUsuarioSeleccionado)!;
+  }
+
+  alSeleccionarUsuario(id: string) {
+    this.idUsuarioSeleccionado = id;
   }
 }
